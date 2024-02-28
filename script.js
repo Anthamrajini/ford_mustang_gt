64b3b87f7b499244bc994a22ccccfac279934334
Anthamrajini <140598441+Anthamrajini@users.noreply.github.com>
2024-02-28 21:44:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu(){
+    const menu = document.getElementById('menu');
+    const nav=document.getElementById('nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
